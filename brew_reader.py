3dd7cca964af0746e6eba81c13991d15d06e83f7
Andrew D. McGuire <amcguire227@gmail.com>
2016-10-21 21:41:53 +0100

    upgrading the reader

--- brew_reader.py
+++ brew_reader.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-'''This file is part of McBrew. It attempts to read the DBdata generate
+'''This file is part of McBrew. It attempts to read the DB data generated
 by brew_recorder.py and plot the data as it comes in
 
 we weant to file ip an array, with say, 100 points, then we want ot just take the last value and delet the array'''
 
 from os.path import expanduser, join, isfile
 from shutil import copyfile
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
 
 def startReader():
 
@@ -19,10 +19,10 @@ def startReader():
     #get the required paths
     home = expanduser("~")
     db_path = join(home,'Dropbox','homebrew','brew_recorder')
-    rec_filenames = [join(db_path,'temperature_evolution_start.txt')]
-    rec_filenames.append(join(db_path,'temperature_evolution_end.txt'))
-    cache_filenames = [join(db_path,'temperature_cache_1.txt')]
-    cache_filenames.append(join(db_path,'temperature_cache_2.txt'))
+    rec_filenames = [join(db_path,'T_cache_1.txt')]
+    rec_filenames.append(join(db_path,'T_cache_2.txt'))
+    cache_filenames = [join(db_path,'T_cache_1.txt')]
+    cache_filenames.append(join(db_path,'T_cache_2.txt'))
 
 
     '''check that the cache files exist'''
@@ -47,10 +47,10 @@ def startReader():
         print "Generating temporary caches."
         copyfile(rec_filenames[0],cache_filenames[0])
         copyfile(rec_filenames[1],cache_filenames[1])  
-        c0_df = pd.read_csv(cache_filenames[0],sep='\t',dtype={'time': "datetime64[ns]", 'temp': np.float64},names=["time","temp"])
-        c1_df = pd.read_csv(cache_filenames[1],sep='\t',dtype={'time': 'datetime64[ns]', 'temp': np.float64},names=["time","temp"])
-        print c0_df.index
-        print c1_df.index
+        # c0_df = pd.read_csv(cache_filenames[0],sep='\t',dtype={'time': "datetime64[ns]", 'temp': np.float64},names=["time","temp"])
+        # c1_df = pd.read_csv(cache_filenames[1],sep='\t',dtype={'time': 'datetime64[ns]', 'temp': np.float64},names=["time","temp"])
+        # print c0_df.index
+        # print c1_df.index
         
     # #check that the cache files exist
     # for i, rec_file in enumerate(rec_filenames):
